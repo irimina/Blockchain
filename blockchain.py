af8b173f9dfa77cb3fe1177b8aeff409e8542fe8
hungryTechie <iirimina@schools.nyc.gov>
2022-04-03 16:47:21 -0400

    blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,27 +1,27 @@
-# Task: Instantiate the blockchain object, create three transactions, add them to the chain and display the chain after each one. 
-
+#Blockhain project
 
 import hashlib
 import json
 from time import time
+import random
 
-#Blockhain Class
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.pending_transactions = []
-        self.new_block(previous_hash="The Times 03/Jan/2009/Chancellor on brink of second bailout for banks", proof=100)
+        self.new_block(100)
 
-# Create a new block listing key/value pairs of block information in a JSON object. Reset the list of pending transactions & append the newest block to the chain.
+# Create a new block listing key/value pairs of block
+#information in a JSON object. Reset the list of pending transactions &
+#append the newest block to the chain.
     def new_block(self, proof, previous_hash=None):
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time(),
             'transactions': self.pending_transactions,
             'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
-             # true hash of the genesis block: 
-        }
+            'previous_hash': previous_hash or self.hash(self.chain[-1]) if self.chain else None,
+                }
         self.pending_transactions = []
         self.chain.append(block)
         return block
@@ -31,7 +31,7 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-# Add a transaction with relevant info to the 'blockpool' - list of pending tx's. 
+# Add a transaction with relevant info to the 'blockpool'
 
     def new_transaction(self, sender, recipient, amount):
         transaction = {
@@ -42,7 +42,8 @@ class Blockchain(object):
         self.pending_transactions.append(transaction)
         return self.last_block['index'] + 1
 
-# receive one block. Turn it into a string, turn that into Unicode (for hashing). Hash with SHA256 encryption, then translate the Unicode into a hexidecimal string.
+# receive one block. Turn it into a string, turn that into Unicode (for hashing).
+#Hash with SHA256 encryption, then translate the Unicode into a hexidecimal string.
 
     def hash(self, block):
         string_object = json.dumps(block, sort_keys=True)
@@ -50,31 +51,28 @@ class Blockchain(object):
         raw_hash = hashlib.sha256(block_string)
         hex_hash = raw_hash.hexdigest()
         return hex_hash
- 
-# this line is outside the class to generate a proof ID
-proofID= random.randrange(100, 100000)
 
-#. Add your code below here #
+# - the random numbers simulate proofs for the 21 million maximum amount of bitcoins
+proof1 = '0000' + str(random.randint(1,21000000))
+proof2 = '0000' + str(random.randint(1,21000000))
+proof3 = '0000' + str(random.randint(1,21000000))
 
+# HW Task: Add three blocks to the chain that record three transactions to three people. Print the chain at each stage.
+
+#Add your code below this line
+####################################################
 # STEP 1 instantiate the blockchain object
 
-# STEP 2 print the empty chain
+# STEP 2 print the empty chain that has the genesis block with proof =100
 
-# STEP 3 initiate the first transation by calling the new_transaction() on the object with the arguments of your choice
+# STEP 3 initiate the first transation of 1BTC from ___ to ___ by calling the new_transaction() on the object
 
-# STEP 4 add the new block to the chain using the new_block() by passing the proofID numberbk_
+# STEP 4 add the new block to the chain using the new_block() by passing a proof argument for each
 
-# STEP 5 print the entire chain 
+# STEP 5 Print the chain with the new changes
 
-# Repeat the steps 3-5 twice to add two more transactions
+# STEP 6 Repeat steps 3-5 to record two more transactions
 
-# NOTE: you can add some print() statements among transactions for easier reading
-
-
-
-
-
-
-
+#Note:(optional) You may add any formating sybmols to show how the chain looks at each phase
 
 
